fdbfafdba85b015a06c0e09e43e1671495f7e9b3
mroatder <nattawat.six@gmail.com>
2024-11-24 10:39:17 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,12 @@
 <template>
-  <div id="app" class="bg-dark text-white">
-    <Header />
-    <HeroSection />
-    <MovieList />
-    <Footer />
+  <div id="app">
+    <!-- ไม่ใส่ Navbar ที่นี่ เพราะจะใส่ในหน้าอื่นๆ -->
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue';
-import HeroSection from './components/HeroSection.vue';
-import MovieList from './components/MovieList.vue';
-import Footer from './components/Footer.vue';
-
 export default {
   name: 'App',
-  components: {
-    Header,
-    HeroSection,
-    MovieList,
-    Footer
-  }
-}
+};
 </script>
-
-<style>
-body {
-  background-color: #121212;
-}
-</style>
